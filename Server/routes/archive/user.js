9c4991080d11c110590cb3686d51f77a634b4469
clee17 <christina.lee.cn@outlook.com>
2020-02-25 16:09:38 +0800

    user request updated

--- Server/routes/archive/user.js
+++ Server/routes/archive/user.js
@@ -255,7 +255,7 @@ let handler = {
                             ],
                             "fanfic_works":[
                                 {$match:{"work.type":{$lt:100},"index.order":0,"work.published":true,infoType:0}},
-                                {$set:{updated:"$work.date"}}
+                                {$set:{"visited":{$sum:"$index.visited"},"book.wordCount":{$sum:"$index.wordCount"},updated:"$work.date"}},
                             ]
                         }},
                     {$project:{all:{$setUnion:["$fanfic_chapter","$fanfic_works"]}}},
@@ -453,8 +453,8 @@ let handler = {
                                     {$match:{$expr:{$eq:["$_id","$$userId"]}}},
                                     {$project:{user:1,_id:1}}]}},
                         {$unwind:"$chapter.user"},
-                        {$lookup:{from:"work_chapters",let:{chapter_id:"$index.chapter"},as:"countChapter",pipeline:[{$match:{$expr:{$eq:["$_id","$$chapter_id"]},published:true}},{$project:{contents:0}}]}},
-                        {$set:{updated:"$work.date",infoType:0}},
+                        {$lookup:{from:"work_chapters",let:{bookId:"$work._id"},as:"countChapter",pipeline:[{$match:{$expr:{$eq:["$book","$$bookId"]},published:true}},{$project:{contents:0}}]}},
+                        {$set:{updated:"$work.date",infoType:0,"work.visited":{$sum:"$countChapter.visited"},"work.wordCount":{$sum:"$countChapter.wordCount"}}},
                         {$project:{countChapter:0}}
                     ]
                 }},
