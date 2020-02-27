07787f497ddd077a9951e57d7d363d01f3a223f2
clee17 <christina.lee.cn@outlook.com>
2020-02-27 20:55:43 +0800

    bugfix2

--- data/model/cleeGame_userSetting.js
+++ data/model/cleeGame_userSetting.js
@@ -1,4 +1,4 @@
-var connect = module.require('../connectors/cleeGame');
+var connect = module.require('../connectors/cleegame');
 var schema = require('../schema/cleeGame_userSetting');
 
 module.exports = connect.model('user_setting',schema,'user_setting');
