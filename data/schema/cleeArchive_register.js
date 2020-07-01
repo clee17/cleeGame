3575724a501ac8f4bd339c3c69a4e4428c6ca921
christina li <christina.lee.cn@outlook.com>
2020-07-01 11:38:19 +0800

    add the modification to database schema

--- data/schema/cleeArchive_register.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = new mongoose.Schema({
-    data : {type:Date,default:Date.now()},
-});
---
+++ data/schema/user_register.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema({
+    mail:{type:String,default:''},
+    secondaryMail:{type:String,default:''},
+    intro:{type:String,default:''},
+    logged:{type:Date,default:Date.now()},
+});
